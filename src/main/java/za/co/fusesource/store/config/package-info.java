266d5ea8e67518c2ebf4c012c4600dbdd5751078
Oshima Tlholoe <ptlholoe@fnb.co.za>
2018-08-09 18:42:02 +0200

    Initial application generated by JHipster-5.1.0

---
+++ src/main/java/za/co/fusesource/store/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package za.co.fusesource.store.config;
---
+++ src/main/java/za/co/fusesource/store/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package za.co.fusesource.store.repository;
+
+import za.co.fusesource.store.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
